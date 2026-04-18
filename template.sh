9f5f963e29f031eb492d45c8ea1c372c7f1085cd
“Enqing07” <enqing0727@gmail.com>
2026-04-18 13:54:20 +0800

    add folder structure

---
+++ template.sh
@@ -0,0 +1,15 @@
+# Create directory
+mkdir -p src 
+mkdir -p research
+
+# Create files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompts.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully!"
