c1d68a1f1a61bf10c34a9e97f343fc88346aa6e0
“Enqing07” <enqing0727@gmail.com>
2026-04-18 15:25:39 +0800

    Add requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical-chatbot',
+    version='0.1.0',
+    author='En Qing',
+    author_email='en.qing0727@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
